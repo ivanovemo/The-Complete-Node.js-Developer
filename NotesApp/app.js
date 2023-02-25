4040ad0dbe560a76947030744728ecce386e728f
Emil <emoemo2535@gmail.com>
2023-02-25 18:04:15 +0200

    First task solution

---
+++ NotesApp/app.js
@@ -0,0 +1,67 @@
+const chalk = require('chalk')
+const { demandOption } = require('yargs')
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removes a note!',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demantOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv){
+        notes.removeNote(argv.title)
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List of all notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads a note',
+    builder: {
+        title: {
+            describe: "Shows note text",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
